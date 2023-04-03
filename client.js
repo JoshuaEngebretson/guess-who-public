fc5ff512f0c571d9870eacd2c3a35bba234caa89
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-03 18:13:16 -0500

    .js and .css files sourced for index.html
    .js changes
    	-Built onReady function
    	-Built function showPeople, when called this loads the GitHub profile
    	   picture for each person in the people array (people array located
    	   within data.js)
    	-added in jQuery.js
    
    .css changes
    	-Styled the background color of the html page to gray
    
    index.html changes
    	-Added header, main, and footer elements.
    	-Added div with id="GitHub-Images" inside of the main element.

--- client.js
+++ client.js
@@ -1,1 +1,22 @@
-console.log('Here are all the available people:', people);
+$(document).ready(onReady)
+
+// all .js files sourced properly
+
+function onReady() {
+  console.log('Here are all the available people:', people);
+
+  showPeople();
+
+}
+
+function showPeople() {
+  console.log('in showPeople');
+
+  for (const person of people) {
+    $('#GitHub-Images').append(`
+      <div>
+      <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of Chris">
+      </div>
+    `)
+  }
+}
