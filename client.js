853ddd641f8c08165fcafa470157a0c3fee2b289
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-03 18:41:43 -0500

    Added randomNumber function to client.js
    -added button to html page to test out randomNumber if it was given an input of
       randomNumber(1, 20)

--- client.js
+++ client.js
@@ -4,8 +4,9 @@ $(document).ready(onReady)
 
 function onReady() {
   console.log('Here are all the available people:', people);
-
   showPeople();
+
+  $('#D20').on('click', d20)
 
 }
 
@@ -14,9 +15,26 @@ function showPeople() {
 
   for (const person of people) {
     $('#GitHub-Images').append(`
-      <div>
+      <div class="Profile-Picture">
       <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of Chris">
       </div>
     `)
   }
+}
+
+
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+console.log(randomNumber(1, 20));
+
+function d20() {
+  let d20Roll = randomNumber(1,20)
+
+  if (d20Roll === 20) {
+    return console.log('you got a crit! ---->', d20Roll);
+  }
+
+ return console.log(d20Roll);
 }
