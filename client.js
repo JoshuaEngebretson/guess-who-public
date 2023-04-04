d6baeb125ae2a21b1871b76a52e5f3ae6096ddd4
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-03 22:27:20 -0500

    Guess Who now works!
    -Built out userGuess function to properly check if the id of what was clicked
       matches the name of the guess who answer and if so runs the GenerateAnswer
       function to start the game over.

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ $(document).ready(onReady)
 
 const GuessWhoMax = (people.length-1)
 let GuessWhoAnswer;
+let guessCount = 1;
 
 // all .js files sourced properly
 function onReady() {
@@ -14,25 +15,23 @@ function onReady() {
 
 }
 
-// Function renders the GitHub profile picture for each person
-//  in the "people" array and assigns that div a data point of
-//  Picture-x where x is the placement within the array.
-function renderProfilePicture() {
 
-  let x = 1
+// Function renders the GitHub profile picture for each person
+//  in the "people" array and assigns an id of that person's name
+function renderProfilePicture() {
   
   for (const person of people) {
-    $('#GitHub-Images').data(`${person.name}`);
     $('#GitHub-Images').append(`
       <div class="Profile-Picture" id="${person.name}">
-        <img src="https://github.com/${person.githubUsername}.png?size=250"
+        <img src="https://github.com/${person.githubUsername}.png?size=300"
         alt="Profile image of ${person.name}">
       </div>
     `)
-    x++
+    $('#person.name').data(`${person.name}`);
   }
 
 }
+
 
 function GenerateAnswer() {
 
@@ -46,22 +45,45 @@ function GenerateAnswer() {
     </p>
   `);
 
-  GuessWhoAnswer = randomPersonName
+  GuessWhoAnswer = randomPersonName;
+  guessCount = 1;
 
 }
 
 
 function userGuess() {
 
-  window.alert('is this working?');
-  // $(this).alert('is this working?');
+  let guess = $(this).attr('id')
+  
+  if (guess === GuessWhoAnswer) {
+    window.alert(`You found ${GuessWhoAnswer}! Let's play again!`)
+    
+    // If correct Generate Answer
+    GenerateAnswer();
+  }
+  else {
+    if (guessCount === 1) {
+      window.alert(`That is not ${GuessWhoAnswer}, try again`)
+    }
+    else if (guessCount === 2) {
+      window.alert(`That is also not ${GuessWhoAnswer}, try again`)
+    }
+    else if (guessCount > 4) {
+      window.alert(`Odds aren't in your favor are they... 
+        \nYou're on guess ${guessCount}, and still haven't found ${GuessWhoAnswer}?Keep trying!
+      `)
+    }
+    else {
+      window.alert(`Maybe you should try guessing someone else...`)
+    }
+    guessCount++
+  }
 
-  // If correct Generate Answer
-  GenerateAnswer();
+
 
 }
+
 
 function randomNumber(min, max){
   return Math.floor(Math.random() * (1 + max - min) + min);
 }
-
